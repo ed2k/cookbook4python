eb22135ae4da0f0af66a501acefdc915300a35a1
sunyin51 <sunyin51@7705c3fb-1738-0410-84f5-8356642dce13>
2008-02-06 03:36:39 +0000

    java applet bridge client

---
+++ trunk/jbridge/aptMine.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import java.net.*;
+
+public class aptMine extends Applet {
+   Button a, b;
+   int level=0;
+   jMine  assoc;
+
+
+  boolean isStandalone = false;
+
+  //Initialize the applet
+  public void init() {
+      setLayout(new BorderLayout());
+
+      assoc = new jMine();
+      add("Center", assoc);
+  }
+   public void destroy() {
+      remove(assoc);
+      //  remove(p);        remove(controls);
+   }
+
+   public static void main(String args[]) {
+      Frame f = new Frame("Mine");
+      aptMine a = new aptMine();
+      a.init();
+      a.start();
+
+      f.add("Center", a);
+      f.setSize(600, 500);
+      f.show();
+   }
+
+  //Get Applet information
+  public String getAppletInfo() {
+    return "Applet Information";
+  }
+
+  //Get parameter info
+  public String[][] getParameterInfo() {
+    return null;
+  }
+
+
+  
+ 
+
+
+}
+
