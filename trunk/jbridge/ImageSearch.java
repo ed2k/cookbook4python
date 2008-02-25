799f1635ddb0f0ca15b3291a47c2db3bbcec7b38
sunyin51 <sunyin51@7705c3fb-1738-0410-84f5-8356642dce13>
2008-02-25 03:58:55 +0000

    sbridge.java ImageSearch.java yahoo bridge card counter

---
+++ trunk/jbridge/ImageSearch.java
@@ -0,0 +1,147 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.awt.Point;
+import javax.imageio.ImageIO;
+
+
+public class ImageSearch {
+	int startx, starty;
+	BufferedImage[] smallCards, bigCards, smallSuits, bigSuits;
+	BufferedImage tableCorner, bidImage;
+	static String trans = "AKQJT98765432"; 
+	static String trans2 = "SHDC";
+	
+	ImageSearch(){
+		String root= "C:\\Documents and Settings\\lmcyisu\\My Documents\\My Pictures\\";
+		smallCards = new BufferedImage[13];
+		bigCards = new BufferedImage[13];
+		smallSuits = new BufferedImage[4];
+		bigSuits = new BufferedImage[4];
+		try{		
+			BufferedImage base = ImageIO.read(new File(root+"cards.png"));
+			for (int i=0;i<13;i++){
+				smallCards[i] = base.getSubimage(i*8, 0, 8, 9);
+				bigCards[i] = base.getSubimage(i*10, 20, 10, 11);
+			}
+			for (int i=0;i<4;i++){
+				smallSuits[i] = base.getSubimage(i*9, 9, 9, 11);;
+				bigSuits[i] = base.getSubimage(i*10, 43, 10, 9);;
+			}
+			tableCorner = base.getSubimage(0, 31, 11, 43-31);
+			bidImage = base.getSubimage(41, 43, 17, 11);
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+	Point findTableCorner(BufferedImage image){
+		return isIn(tableCorner,image,0,0,image.getWidth(),image.getHeight(),new PixelMatch());
+	}
+	Point findBidImage(BufferedImage image){
+		return isIn(bidImage,image,0,0,image.getWidth(),image.getHeight(),new PixelMatch());
+	}	
+	char findBigCard(BufferedImage image){
+		return findBigCard(image,0,0,image.getWidth(),image.getHeight(), new MatchBlue());
+	}
+	char findBigSuit(BufferedImage image){
+		for (int i=0;i<bigSuits.length;i++){
+			if (isIn(bigSuits[i], image, 0, 0, image.getWidth(), image.getHeight(), new MatchBlue()) != null)
+				return trans2.charAt(i);	
+		}
+		return 0;		
+	}
+	char findBigCard(BufferedImage image ,int initx,int inity, int width, int height, PixelOpts op){
+		for (int i=0;i<bigCards.length;i++){
+			if (isIn(bigCards[i], image, initx, inity, width, height, op) != null)
+				return trans.charAt(i);	
+		}
+		return 0;
+	}
+	int grey(int rgb){
+		int r = (rgb & 0x00ff0000) >> 16;
+		int g = (rgb & 0x0000ff00) >> 8;
+		int b = rgb & 0x000000ff;
+		int a =  33+(126-33)*(r+g+b/3)/255 ;
+		return (a << 16) | (a << 8) | a;
+	}
+	int getB(int rgb){
+		return rgb & 0x000000ff;
+	}
+	char scale2ascii(int rgb){
+		int g = grey(rgb);
+		char c = (char)(g& 0xff);
+		return c;
+	}
+	private void find(String[] src, BufferedImage image ) {
+		// TODO Auto-generated method stub
+		for(int y=0;y<(image.getHeight()-src.length);y++){
+			StringBuffer sb = new StringBuffer();
+			for(int x=0;x<image.getWidth();x++){
+				int p = image.getRGB(x, y);
+				sb.append(scale2ascii(p));
+			}
+			String s = sb.toString();
+			int idx = s.indexOf(src[0]); 
+			if (idx == -1) continue;
+			if (!matchAscii(src,image,idx,y))continue;
+			System.out.println("->"+idx+","+y);
+			startx = idx;
+			starty = y;
+		}			
+	}
+
+	public Point isIn(BufferedImage src, BufferedImage image, PixelOpts op) {
+		return isIn(src, image, 0,0, image.getWidth(),image.getHeight(), op);
+	}
+	class MatchBlue implements PixelOpts {
+
+		@Override
+		public boolean match(int a, int b) {
+			// TODO Auto-generated method stub
+			return getB(a) == getB(b);
+		}
+		
+	}
+	class PixelMatch implements PixelOpts{
+
+		@Override
+		public boolean match(int a, int b) {
+			// TODO Auto-generated method stub
+			return a == b;
+		}
+		
+	}
+	public Point isIn(BufferedImage src, BufferedImage image, int initx,int inity, int width, int height, PixelOpts op){
+		if (src.getHeight()> height || src.getWidth()>width)return null;
+		if ((initx+src.getWidth())> image.getWidth() || (inity+src.getHeight())> image.getHeight()) return null;
+		for(int y=inity; y<(inity+height-src.getHeight());y++){
+			for(int x=initx; x<(initx+width-src.getWidth());x++){
+				if (match(src,image,x,y, op))return new Point(x,y);
+				//System.out.println("->"+idx+","+y);				
+			}
+		}				
+		return null;
+	}
+
+	private boolean match(BufferedImage src, BufferedImage image, int startx, int starty, PixelOpts op) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<src.getWidth();i++)for(int j=0;j<src.getHeight();j++){
+			if(!op.match(src.getRGB(i, j),image.getRGB(startx+i, starty+j))) return false;
+		}
+		return true;
+	}
+
+	private boolean matchAscii(String[] src, BufferedImage image, int startx, int starty) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<src[0].length();i++)for(int j=0;j<src.length;j++){
+			int y = starty + j;
+			if (y > image.getHeight())return false;
+			int p = image.getRGB(startx+i, y);
+			char c = scale2ascii(p);
+			if (src[j].charAt(i) != c) return false;
+		}
+		return true;
+	}
+
+}
+
+interface PixelOpts {
+	boolean match(int a, int b);
+}
