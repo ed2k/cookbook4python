df04a032089f6bded9d58ea3566bebd6cc5409a9
sunyin51 <sunyin51@7705c3fb-1738-0410-84f5-8356642dce13>
2008-04-15 18:11:30 +0000

    read pcap directly in python

--- tools/pcap_reader.py
+++ tools/pcap_reader.py
@@ -1,5 +1,6 @@
 import os,sys
 import struct
+import dpkt
 
 #use dpkt instead, test outdated
 
@@ -22,21 +23,48 @@ hdr = f.read(20)
 vermaj,vermin,tz,sig,snaplen,linktype = struct.unpack(endian+"HHIIII",hdr)
 print endian,vermaj,vermin,tz,sig,snaplen,linktype
 #self.LLcls = LLTypes.get(linktype, Raw)
+UDP = 0x11
+GTP_U_PORT = 2152
+T_PDU = 0xff
 
+sender_cnt = 0
+cnt = 0
 
 hdr = f.read(16)
 while len(hdr) == 16:
     sec,usec,caplen,olen = struct.unpack(endian+"IIII", hdr)
     s = f.read(caplen)
+    # read next packet first
+    hdr = f.read(16)
     # assume ethernet packet
-    dst,src,elen = struct.unpack(endian+'6s6sH',s[:14])
+    if len(s) < 14: continue
+    dst,src,etype = struct.unpack(endian+'6s6sH',s[:14])
+    if etype != 0x800: continue
     #fcs = struct.unpack(endian+'ssss',s[-4:])
     #print str2mac(dst), str2mac(src), 
     ip = s[14:-4]
     ver_ihl,tos,iplen, id,flags_offset,ttl,proto,checksum,src,dst = struct.unpack('>ssH2s2ssB2s4s4s',ip[:20])
+    if src[:3] == '\x0a\xc8\x04' and src[3] in ['\x03','\x04','\x05']:
+        sender_cnt += 1
 
     #data = ip[24:]
-    if proto == 6:
-        print d2ip(src),d2ip(dst),iplen,('%x' % proto)
-    #print data
-    hdr = f.read(16)
+    if proto != UDP: continue
+    #print d2ip(src),d2ip(dst),iplen,('%x' % proto)
+
+    udp = ip[20:]
+    src,dst,size,checksum = struct.unpack('>HHHH',udp[:8])    
+    #print src, dst
+    if dst != GTP_U_PORT: continue
+
+    gtp = udp[8:]
+    flags,etype,length,teid = struct.unpack('>ssH4s',gtp[:8])
+    #print [etype],[teid]
+    if etype != '\xff': continue
+
+    ip = gtp[8:]
+    ver_ihl,tos,iplen, id,flags_offset,ttl,proto,checksum,src,dst = struct.unpack('>ssH2s2ssB2s4s4s',ip[:20])
+    if src[:3] == '\x0a\xc8\x04' and src[3] in ['\x03','\x04','\x05']:
+        cnt += 1
+    
+
+print cnt, sender_cnt
