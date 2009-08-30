53fb77bd6ab36c5ff3841de1246634e7e47b4513
sunyin51@gmail.com <sunyin51@gmail.com@7705c3fb-1738-0410-84f5-8356642dce13>
2009-08-30 04:31:40 +0000

    read dir, calc size of each dir output a depth, fullpath size format

--- trunk/tools/dir2compact.py
+++ trunk/tools/dir2compact.py
@@ -1,88 +1,57 @@
 import sys, os
 
-def extmap(fname):
-    a,ext = os.path.splitext(fname)
+class Node:
+  def __init__(self,name):
+    self.child = {}
+    self.size = 0
+    self.parent = None
+    self.level = 0
+    self.name = name
 
-    ext = ext.upper()
-    if len(ext)<2:
-        return '100'
-    ext = ext[1:]
 
-    if ext in ['TXT','CSV','C','PY','LOG']: return '200'
-    if ext in ['PDF','DOC','DOCX','XLS']: return '180'
-    if ext in ['ISO','ERL','CAB','MDB']: return '-120'  
-    if ext in ['EXE','JAR','OBJ','SYS','CLASS','DLL']: return '-200'
-    if ext in ['AVI','MOV','MPEG','MKV','RMVB','RM','WMV','FLV','3GP']: return '140'
-    if ext in ['MP3','JPG','IMG','PNG']: return '160'
-    if ext in ['GZ','ARJ','ZIP']: return '-110'
-    if ext in ['TAR','PV','vmdk']: return '-100'
-    return '0'
-
-def tr(name): return name.replace('&','&amp;')
-def tag(name,value):
-    return ''.join(['<',name,'>',value,'</',name,'>'])
-def walk(root,dir,depth):
-    path = os.path.join(root,dir)
-    sub_leaves = 0
-    sub_nodes = 0
-    total_leaves = 0
-    total_nodes = 0
-    total_size = 0
-    if os.path.isfile(path):
-        total_leaves += 1
-        print ' '*depth+'<leaf>'
-        print ' '*depth+tag('label',tr(dir))
-        size = os.path.getsize(path)
-        total_size += size
-        print ' '*depth+tag('weight',str(size))
-        print ' '*depth+tag('value',extmap(dir))
-        print ' '*depth+'</leaf>'
-    elif os.path.isdir(path):
-        total_nodes += 1
-        print ' '*depth+'<branch>'
-        print ' '*depth+tag('label',tr(dir))
-        # list files first, dirs next      
-        for p, dirpaths, fnames in os.walk(path):
-          for d in fnames:
-            tnodes,tleaves,sz = walk(path,d,depth+1)
-            total_nodes += tnodes
-            total_leaves += tleaves
-            total_size += sz
-          for d in dirpaths:
-            tnodes,tleaves,sz = walk(path,d,depth+1)            
-            total_nodes += tnodes
-            total_leaves += tleaves
-            total_size += sz
-          sub_nodes = len(dirpaths)
-          sub_leaves = len(fnames)
-          break
-        #print ' '*depth+tag('value',str(total_size))                
-        print ' '*depth+'</branch>'
-    return total_nodes,total_leaves, total_size
-def check_output(f):
-    depth = 0
-    for line in file(f):
-        line = line.strip()
-        if line.find('<node') == 0:
-            print depth,depth*' '+line
-            depth += 1
-        elif line.find('</node') == 0: 
-            depth -= 1
-            print depth,depth*' '+line
-        elif line.find('< nsize')==0:
-            print depth,depth*' '+line
-        #elif line.find('<name=') == 0: print depth*' '+line
-def main ():
+def print_node(n):
+  print n.level,n.name,n.size
+  if len(n.child) > 0:
+     for k,v in n.child.items():
+        print_node(v)
+        
+def walk_dir ():
     root_dir = sys.argv[1]
-    
+    lines = []
     if root_dir[-1] == '\\': root_dir = root_dir[:-1]
     if root_dir[-1] == '/': root_dir = root_dir[:-1]
     head, tail = os.path.split(root_dir)
+    for fullp,dirnames,fnames in os.walk(root_dir,topdown=False):
+      head,tail = os.path.split(fullp)
+      sz = 0
+      for f in fnames:
+          p = os.path.join(fullp,f)
+          if os.path.isfile(p): 
+            sz += os.path.getsize(p)
+      lines.append( (fullp,sz))
+    sum_dir_size(lines)
+    return
 
-    #tn, tl, sz = walk(head,tail,0)
-    for path,dirnames,fnames in os.walk(root_dir,topdown=False):
-        print path
+        
+def sum_dir_size (lines):
+    nodes =  []
+    root_node = Node('/')
+    prev = ""
+    for fullp,sz in lines:
+      f = line.split()
+      sz = int(f[-1])
+      names = fullp.split('/')
+      pt = root_node 
+      for n in names[1:]:
+        if n not in pt.child.keys():
+           pt.child[n]=Node(n)
+           pt.child[n].parent = pt
+           pt.child[n].level = pt.level+1
+        pt.child[n].size += sz
+        pt = pt.child[n]
+    print_node(root_node)
+    return
 
-#if __name__ == '__main__':
-main()
 
+if __name__ == '__main__':
+    walk_dir()
